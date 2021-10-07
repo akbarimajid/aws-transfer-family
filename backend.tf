3aa963aa639c1b6c416eba672dfa19a1a679cf75
akbarimajid <62008510+akbarimajid@users.noreply.github.com>
2021-10-07 22:54:07 +0330

    Sftp user (#3)
    * SFTP USER: add sftp user
    
    * SFTP USER: fix module location
    
    * SFTP USER: fix map syntax
    
    * SFTP USER: fix varialbe
    
    * SFTP USER: fix module
    
    * SFTP USER: add sftp-server
    
    * SFTP USER: fix user module
    
    * delete S3 backend
    
    Co-authored-by: majid.akbari <majid.akbari@snapp.cab>

--- backend.tf
+++ backend.tf
@@ -1,17 +1,9 @@
 terraform {
-  required_version = ">=0.12.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
-  }
-  backend "s3" {
-    bucket         = "majid-bucket-tf-state"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "majid-table-tf-state"
-    encrypt        = true
   }
 }
 
---
+++ modules/aws-sftp-user/variable.tf
@@ -0,0 +1,18 @@
+variable "username" {
+  default = ""
+}
+
+variable "sshkey" {
+  default = ""
+}
+variable "sftp_server_id" {
+  default = ""
+}
+
+variable "s3_bucket_arn" {
+  default = ""
+}
+
+variable "s3_bucket_name" {
+  default = ""
+}
